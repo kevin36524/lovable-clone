c3d5a844839e0392e2024b89ed150b17124a52d6
Kevin Patel <kevin36524@gmail.com>
2025-11-30 07:39:11 -0800

    Just rearraged a bit

--- lovable-ui/.next/server/server-reference-manifest.js
+++ lovable-ui/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TO2slYHRYq2qaUTGHu5Z7r15BZUtVo2On3/kjzTHUQ8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"04VufGcXwIFmJalJARUeB5p5tzECDJ9IE7Ng02MgqXk=\"\n}"
